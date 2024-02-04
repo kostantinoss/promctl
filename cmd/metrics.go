aff68680c1e7b36b2bb5f58a3944d15b3406adbc
Konstantinos Chondralis <khondralis@gmail.com>
2024-02-04 16:41:53 +0200

    add metrics command

--- cmd/metrics.go
+++ cmd/metrics.go
@@ -4,36 +4,51 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"time"
 
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+	"github.com/prometheus/common/model"
 	"github.com/spf13/cobra"
 )
 
 // metricsCmd represents the metrics command
 var metricsCmd = &cobra.Command{
 	Use:   "metrics",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "Prints the available metrics for the configured Prometheus db",
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("metrics called")
+		fmt.Println("hi")
+		server, _ := getDerverFromConfig()
+		fmt.Println("hi2")
+		client, err := api.NewClient(api.Config{
+			Address: server,
+		})
+		if err != nil {
+			fmt.Printf("Error creating client: %v\n", err)
+		}
+
+		api := v1.NewAPI(client)
+
+		result, _, err := api.Query(
+			context.TODO(), "{__name__!=\"\"}", time.Now(), v1.WithTimeout(10*time.Second),
+		)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		if result != nil && result.Type() == model.ValVector {
+			for _, elem := range result.(model.Vector) {
+				fmt.Println(elem)
+			}
+		}
 	},
 }
 
 func init() {
-	getCmd.AddCommand(metricsCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// metricsCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// metricsCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(metricsCmd)
 }
